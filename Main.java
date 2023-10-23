85d223004328f4acff1cedf117369f9e4bef8c9c
jaimanm <jaimancoding@gmail.com>
2023-10-22 22:32:10 -0400

    first commit

--- Main.java
+++ Main.java
@@ -105,7 +105,7 @@ JFrame f = new JFrame("AllerGys");
     
     
     JLabel outputs = new JLabel("You can eat...");
-    outputs.setBounds(300,75,200,25);
+    outputs.setBounds(300,75,200,1);
     outputs.setBackground(Color.blue);
     f.add(outputs);
 
